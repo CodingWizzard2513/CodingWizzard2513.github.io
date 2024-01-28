58de2fd04fd6df53db88107c5de9ab2a3170388b
Andrew Malnic <andrewmalnic@gmail.com>
2024-01-28 10:55:36 -0500

    Added files for ~ Grand Star ~
    Added SMW ModPack Zip For ~ Grand Star ~

--- JS/Content.js
+++ JS/Content.js
@@ -1,3 +1,12 @@
 function NSOpen() {
 	open("./Projects/Nitro-Shop", "_self");
-}
+};
+
+function click() {
+  document.write("EEP");
+};
+
+
+document.getElementById('button').onclick = function() {
+   alert("button was clicked");
+};
