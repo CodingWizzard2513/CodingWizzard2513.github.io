0704ce8f1e463a5c3f815ddf35daf0d4e69d44ca
Anonymous <114874022+CodingWizzard2513@users.noreply.github.com>
2025-05-21 17:51:16 -0400

    Update SKA_Loader.js

--- JS/SKA_Loader.js
+++ JS/SKA_Loader.js
@@ -4,6 +4,13 @@ function LogIn() {
 	var HTML = source + "login.html";
 	var LogInPage = window.open(HTML, "_blank", "width=400,height=500");
 	window.addEventListener('message', (event) => {
-          console.log('Received data:', event.data);
-    });
+        	console.log('Received data:', event.data);
+		if(event.data.type == "signup") {
+			if(!event.data.email = "") {
+				if(event.data.email.includes("@k12.vcsdny.org")) {
+					LogInPage.postMessage({ type: 'error', error: 'school' }, '*');
+				}
+			}
+		}
+    	});
 }
