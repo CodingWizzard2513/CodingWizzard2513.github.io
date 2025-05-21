e62ee3338a986c5f19cbcab69d245737fc877620
Anonymous <114874022+CodingWizzard2513@users.noreply.github.com>
2025-05-21 17:41:12 -0400

    SKA Loader
    For Apache Server

---
+++ JS/loader.js
@@ -0,0 +1,9 @@
+var source = window.location.origin + "/Libraries/SirKingAccount/";
+
+function LogIn() {
+	var HTML = source + "login.html";
+	var LogInPage = window.open(HTML, "_blank", "width=400,height=500");
+	window.addEventListener('message', (event) => {
+          console.log('Received data:', event.data);
+    });
+}
